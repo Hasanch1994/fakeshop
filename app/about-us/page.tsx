05129c2a9b526b2b7df9a7d1ee7f2a05cf7df743
Hasanch1994 <hasan.cheraghi1994@gmail.com>
2023-07-31 18:38:25 +0330

    fix issues

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const AboutUs = async () => {
   return (
-    <main className="w-full h-screen flex justify-center items-start  pt-16">
+    <main className="w-full h-screen flex justify-center items-start  pt-16 px-6 lg:px-4 2xl:px-0 ">
       <div className="w-full flex flex-col gap-y-16  items-center max-w-5xl">
         <div className="mt-16 flex flex-col gap-y-6">
           <h1 className="text-neutral-800 text">
@@ -16,7 +16,7 @@ const AboutUs = async () => {
           </p>
         </div>
 
-        <div className="relative 2xl:w-[800px] h-[558px] lg:w-[650px] w-full rounded-xl">
+        <div className="relative 2xl:w-[800px] lg:h-[558px] lg:w-[650px] w-full h-[250px] rounded-xl">
           <Image
             src={
               "https://www.betterup.com/hubfs/Blog%20Images/Motivating%20a%20team/motivating-a-team-woman-at-meeting-smiling.jpg"
