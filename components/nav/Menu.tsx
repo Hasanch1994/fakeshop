2cac6283d0eaa1e532a235ca4a81245a44572f6b
Hasanch1994 <hasan.cheraghi1994@gmail.com>
2023-07-31 16:20:35 +0330

    add about page

--- components/nav/Menu.tsx
+++ components/nav/Menu.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface menuStyle {
   style: string;
   ulStyle?: string;
@@ -8,10 +10,14 @@ const Menu = ({ style, ulStyle }: menuStyle) => {
       <ul
         className={`list-none menu menu-compact dropdown-content bg-white lg:bg-transparent  ${ulStyle} bg-base-100 rounded-box w-52 flex flex-col  md:w-auto md:flex-row text-dark_ items-center gap-x-3 text-sm font-semibold`}
       >
-        <li className="menuItemStyle">Shop</li>
+        <Link href={"/home"}>
+          <li className="menuItemStyle">Shop</li>
+        </Link>
         <li className="menuItemStyle">Collective</li>
         <li className="menuItemStyle">Designers</li>
-        <li className="menuItemStyle">About</li>
+        <Link href={"/about-us"}>
+          <li className="menuItemStyle">About</li>
+        </Link>
         <li className="menuItemStyle">Contact</li>
       </ul>
     </nav>
